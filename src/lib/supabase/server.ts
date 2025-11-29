faeb1af9196f5196e8b8a4e79580a9f172d32124
Mohammed Faisal <mdfaisal.2k5@gmail.com>
2025-11-29 12:02:31 +0000

    Error: Invalid supabaseUrl: Must be a valid HTTP or HTTPS URL.     at va

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -21,7 +21,7 @@ type Database = {
   };
 };
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
